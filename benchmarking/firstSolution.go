5d269f4a4d7ec37e58e3e74206521d913c2f03ce
MacBobby Chibuzor <theghostmac@gmail.com>
2022-03-29 18:58:09 +0100

    benchmark code

--- benchmarking/firstSolution.go
+++ benchmarking/firstSolution.go
@@ -1,1 +1,22 @@
 package benchmarking
+
+import "os"
+
+func LengthOfFile(f string, bufferSize int) (int, error) {
+	f, err := os.Open(f)
+	if err != nil {
+		return 0, err
+	}
+	defer f.Close()
+
+	count := 0
+	for {
+		buffer := make([]byte, bufferSize)
+		num, err := f.Read(buffer)
+		count += num
+		if err != nil {
+			break
+		}
+	}
+	return count, nil
+}
