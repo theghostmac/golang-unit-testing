56e15c45a9ff6c4315aa84c56329ca8fffc82437
MacBobby Chibuzor <theghostmac@gmail.com>
2022-03-29 14:45:44 +0100

    unit test for addTwoNumber function

--- adder/addition_test.go
+++ adder/addition_test.go
@@ -1,1 +1,10 @@
 package adder
+
+import "testing"
+
+func Test_addTwoNumbers(t *testing.T) {
+	test_result := addTwoNumbers(10, 15)
+	if test_result != 25 {
+		t.Error("Results are not correct: expected 25, got ", test_result)
+	}
+}
