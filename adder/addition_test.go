97b55e44e85853158e403201b3e5c96eb7c7240d
MacBobby Chibuzor <theghostmac@gmail.com>
2022-03-29 15:16:16 +0100

    adder function has an api testing package now

--- adder/addition_test.go
+++ adder/addition_test.go
@@ -3,8 +3,8 @@ package adder
 import "testing"
 
 func Test_AddTwoNumbers(t *testing.T) {
-	test_result := AddTwoNumbers(10, 15)
-	if test_result != 25 {
-		t.Error("Results are not correct: expected 25, got ", test_result)
+	testResult := AddTwoNumbers(10, 15)
+	if testResult != 25 {
+		t.Error("Results are not correct: expected 25, got ", testResult)
 	}
 }
--- adderPublic_test/adder_public_test.go
+++ adderPublic_test/adder_public_test.go
@@ -1,1 +1,13 @@
 package adderPublic_test
+
+import (
+	"github.com/theghostmac/golang-unit-testing/adder"
+	"testing"
+)
+
+func Test_addTwoNumbers(t *testing.T) {
+	testResult := adder.AddTwoNumbers(10, 15)
+	if testResult != 5 {
+		t.Error("Result is incorrect: expected 5, got ", testResult)
+	}
+}
