f75958e69254b85928532278854d31902bd4493b
MacBobby Chibuzor <theghostmac@gmail.com>
2022-03-29 15:17:48 +0100

    complete unit testing

--- adderPublic_test/adder_public_test.go
+++ adderPublic_test/adder_public_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func Test_addTwoNumbers(t *testing.T) {
+func Test_AddTwoNumbers(t *testing.T) {
 	testResult := adder.AddTwoNumbers(10, 15)
 	if testResult != 5 {
 		t.Error("Result is incorrect: expected 5, got ", testResult)
