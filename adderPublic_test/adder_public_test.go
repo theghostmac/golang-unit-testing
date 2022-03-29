16430d27b6022c94d5faae9bb244578f9722d942
MacBobby Chibuzor <theghostmac@gmail.com>
2022-03-29 16:53:51 +0100

    new package for table test demo

--- adderPublic_test/adder_public_test.go
+++ adderPublic_test/adder_public_test.go
@@ -7,7 +7,7 @@ import (
 
 func Test_AddTwoNumbers(t *testing.T) {
 	testResult := adder.AddTwoNumbers(10, 15)
-	if testResult != 5 {
+	if testResult != 25 {
 		t.Error("Result is incorrect: expected 5, got ", testResult)
 	}
 }
