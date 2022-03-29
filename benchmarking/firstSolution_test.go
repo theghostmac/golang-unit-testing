03d3a5cd490b131d1c4622c7dec142256d75287e
MacBobby Chibuzor <theghostmac@gmail.com>
2022-03-29 19:14:38 +0100

    test completed

--- benchmarking/firstSolution_test.go
+++ benchmarking/firstSolution_test.go
@@ -3,5 +3,11 @@ package benchmarking
 import "testing"
 
 func TestLengthOfFile(t *testing.T) {
-	testResult, err := LengthOfFile("")
+	testResult, err := LengthOfFile("sample.txt", 1)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if testResult != 500 {
+		t.Error("Expected 'Just Random Text', got ", testResult)
+	}
 }
